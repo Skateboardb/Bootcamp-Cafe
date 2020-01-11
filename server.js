3b3fb309d9748a6917b0d193d73f93b6bd985230
coltonfalkner <coltonfalkner@gmail.com>
2020-01-11 13:43:12 -0500

    slight mods to server and reserve.html

--- server.js
+++ server.js
@@ -13,37 +13,46 @@ var waitlist = [];
 //home
 //tables
 //reserve
-app.listen(PORT, function(){
-    console.log("Server listening on post: " + PORT);
-})
-
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "view/home.html"));
+app.listen(PORT, function() {
+  console.log("Server listening on post: " + PORT);
 });
 
-app.get("/reserve", function(req, res){
-    res.sendFile(path.join(__dirname, "view/reserve.html"));
-});
-//post function 
-
-app.get("/table", function (req, res){
-    res.sendFile(path.join(__dirname, "view/tables.html"));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "view/home.html"));
 });
 
-app.get("/api/tables", function(req, res){
-    res.sendFile(path.join(__dirname, "api/reserved.html"));
-    res.json(reservations);
-})
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "view/reserve.html"));
+});
 
-app.get("/api/waitlist", function(req, res){
-    res.sendFile(path.join(__dirname, "api/waitlist.html"));
-    res.json(waitlist);
-})
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "view/tables.html"));
+});
 
-app.post("/reserve" && "/api/reserved", function(req, res){
+app.get("/api/tables", function(req, res) {
+  res.json(reservations);
+});
+
+app.get("/api/waitlist", function(req, res) {
+  res.sendFile(path.join(__dirname, "api/waitlist.html"));
+  res.json(waitlist);
+});
+
+app.post("/reserve", function(req, res) {
+  var newReservation = req.body;
+  console.log(newReservation);
+  if (reservations.length < 5) {
+    reservations.push(newReservation);
+    res.json(true);
+  } else {
+    waitlist.push(newReservation);
+    res.json(false);
+  }
+});
+
+app.post("/api/reserved", function(req, res){
     var newReservation = req.body;
-    console.log(newReservation);
-    if (reservations.length < 5){
+    if(reservations.length < 5){
         reservations.push(newReservation);
         res.json(true);
     }else{
@@ -51,14 +60,3 @@ app.post("/reserve" && "/api/reserved", function(req, res){
         res.json(false);
     }
 })
-
-// app.post("/api/reserved", function(req, res){
-//     var newReservation = req.body;
-//     if(reservations.length < 5){
-//         reservations.push(newReservation);
-//         res.json(true);
-//     }else{
-//         waitlist.push(newReservation);
-//         res.json(false);
-//     }
-// })
